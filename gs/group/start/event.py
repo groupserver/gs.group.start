3a5f8a3de16878778d3f6afdb7847eb8a1032f6d
Michael JasonSmith <mpj17@onlinegroups.net>
2012-03-29 13:07:43 +1100

    Clean-up of the imports

--- gs/group/start/event.py
+++ gs/group/start/event.py
@@ -1,16 +1,11 @@
 # coding=utf-8
 from zope.interface import implements, implementedBy
-from OFS.Folder import Folder
 from zope.component.interfaces import ObjectEvent
 from zope.component.interfaces import IObjectEvent
 
-from Products.XWFChat.interfaces import IGSGroupFolder
-
 class IGSGroupCreatedEvent(IObjectEvent):
-    """ An event issued after a group has been created, and all core changes
-        to the group have been made.
-
-    """
+    """ An event issued after a group has been created, and all core 
+        changes to the group have been made."""
 
 class GSGroupCreatedEvent(ObjectEvent):
     implements(IGSGroupCreatedEvent)
