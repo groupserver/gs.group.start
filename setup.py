9615e5640fa601e15811adbc26904c9bbcfca086
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-30 16:34:17 -0700

    Following jQuery to its new home

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -40,7 +40,7 @@ setup(name='gs.group.start',
         'zope.interface',
         'zope.schema',
         'Zope2',
-        'gs.content.js.jquery',
+        'gs.content.js.jquery.base',
         'gs.content.layout',
         'gs.content.form',
         'gs.group.base',
